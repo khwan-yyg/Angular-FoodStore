91eee098702f260d6e892cc6423a78bed31b5637
Khwankamon <khwan_yyg@outlook.com>
2024-02-03 23:05:47 +0700

    Finished Part 5

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/partials/header/header.component";
+import { HomeComponent } from "./components/pages/home/home.component";
+import { StarRatingComponent } from "./components/partials/star-rating/star-rating.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    StarRatingComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
